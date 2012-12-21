598c0dd51a7cf328dc9c8508090e3d9f4b027a1c
Isaac Shivvers <ishivvers@berkeley.edu>
2012-12-21 14:34:28 -0600

    working version of estimate_y

--- estimate_y.py
+++ estimate_y.py
@@ -21,85 +21,121 @@ good_obs = dec_obs[ dec_obs[:,-2] < 20. ]
 # first, produce photometry for only those that cross-match
 #  to 2MASS
 
-from get_SEDs import _find_field, _split_field, _get_reddening  #need to import helpers explicitly
+from get_SEDs import _find_field, _split_field  #need to import helpers explicitly
 from get_SEDs import *
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def get_Y_wth_match( object_coords, DEC_obs, err_cut=.5, redden=False, max_size=900., plot=False):
+def get_Y_match( object_coords, DEC_obs, max_size=900., plot=False):
     '''
     Estimate y-band photometry for sources that cross-match to 2MASS
     
-    DEC_obs: array of [[g, g_err, r, r_err, i, i_err, z, e_err], ...]
+    DEC_obs: array of [[u, u_err, g, g_err, r, r_err, i, i_err, z, z_err, y, y_err], ...]
     '''
-    field_center, field_width = _find_field( object_coords )
+    
+    # only bother with sources z<20., since any dimmer than that will not be in
+    #  the 2MASS catalog
+    good_obs, good_coords = [],[]
+    for i,row in enumerate(DEC_obs):
+        if row[8]<20.:
+            good_obs.append( (row,i) ) #keep track of modeled sources original indices
+            good_coords.append( object_coords[i] )
+    good_coords = np.array(good_coords)  #keep good_obs as list, because of the indexer
+    
+    field_center, field_width = _find_field( good_coords )
     # split field and query/match for each tile
     #  works even if we don't need to split
-    centers, tile_width = _split_field( field_center, field_width, max_size, object_coords=object_coords )
+    centers, tile_width = _split_field( field_center, field_width, max_size, object_coords=good_coords )
     
     print 'Matching to 2MASS...'
     # go through each tile and accumulate the results:
-    obj_coords, obj_mags, = [],[]
+    matched_coords, matched_obs, matched_indexer = [],[],[]
     for center in centers:
         mass = query_2mass( center[0], center[1], boxsize=tile_width )
         if mass != None:
             # produce list of matched objects
-            matches = identify_matches( mass[:,:2], object_coords )
+            #  If can figure a better way to track indices, could make this faster by
+            #  only attempting to match to sources in this queried field.
+            matches = identify_matches( mass[:,:2], good_coords )
             if matches != None:
                 for i,obj in enumerate(mass):
                     if matches[i] != None:
                         i_match = matches[i]
-                        obj_mags.append( np.hstack( (DEC_obs[i_match], obj[2:]) ) )
-                        obj_coords.append( object_coords[i_match] )
+                        i_orig = good_obs[i_match][1]
+                        if i_orig in matched_indexer:
+                            # we already matched this source. continue for now,
+                            #  perhaps there's a more graceful way to handle this?
+                            continue
+                        matched_indexer.append( i_orig )
+                        mobs = np.hstack( (good_obs[i_match][0], obj[2:]) )
+                        matched_obs.append( mobs )
+                        matched_coords.append( good_coords[i_match] )
     
-    # now go through the matched sources and model each, predicting the Ymag
-    out_coords, out_mags, Ys, errors = [],[],[],[]
+    # now go through the matched sources and model each, predicting the missing mags
+    modeled_mags, modeled_errs = [],[]
     print 'Modeling matches...'
-    for i, obs in enumerate(obj_mags):
-        if i%100==0: print i
+    for i, obs in enumerate(matched_obs):
+        if i%10==0: print i
         
-        mask = np.array( [0,1,1,1,1, 0, 0,0, 1,1,1] ).astype(bool) #expected observations
-        # if any of these are not observed (value == 99.), mask them as well
-        ##  DOES NOT WORK DOES NOT WORK
-        mask[mask][ obs[::2] == 99. ] = False
+        good_obs = []
+        mask = [1,1,1,1,1, 0, 0,0, 1,1,1] #expected
+        # if any of the orignal obs are not actually observed (value == 99.), mask them as well
+        #  this builds up an array of good observations and associated errors
+        for j,val in enumerate(obs[::2]):
+            if val == 99.:
+                mask[j] = 0 # this is irrelevant for the 2MASS obs, i.e. j>6
+            else:
+                good_obs += [val, obs[1::2][j]]
+        good_obs = np.array(good_obs)
+        mask = np.array(mask).astype(bool)
         
-        if redden:
-            reddening = _get_reddening( obj_coords[i][0],obj_coords[i][1], ALL_FILTERS )
-            # de-redden the observations before comparing
-            #  to the models
-            obs[::2] -= reddening[mask]
-        
-        model, T, err = choose_model( obs, mask )
+        model, T, err = choose_model( good_obs, mask )
 
-        if err > err_cut: #apply quality-of-fit cut
-            continue
-        else:
-            if redden:
-                # re-redden the model and observations
-                obs[::2] += reddening[mask]
-                model += reddening
-            
-            # return Ymag, and keep track of errors
-            Ys.append(model[5])
-            errors.append( err )
-            out_coords.append( obj_coords[i] )
-            out_mags.append( obs )
-
-            if i<10 and plot:
-                plt.scatter( MODELS[0][1:][mask], obs[::2], c='g' )
+        final_mags, final_errs = [],[]
+        for j,val in enumerate(obs[::2][:6]): #trim 2MASS mags here, with the :6 cut
+            if val == 99.:
+                #replace this value with modeled
+                final_mags.append( model[j] )
+                final_errs.append( err )
+            else:
+                #keep the observed value
+                final_mags.append( val )
+                final_errs.append( obs[1::2][j] )
+        
+        final_mags = np.array(final_mags)
+        final_errs = np.array(final_errs)
+        modeled_mags.append( final_mags )
+        modeled_errs.append( final_errs )
+        
+        # now plot up a few, to see how they look:
+        if plot:
+            if i<plot:
+                plt.scatter( MODELS[0][1:][mask], good_obs[::2], c='r' )
                 plt.scatter( MODELS[0][1:], model, c='k' )
-                plt.scatter( MODELS[0][1:][5], model[5], c='r' )
                 plt.xlabel('wavelength')
                 plt.ylabel('mag')
                 plt.title('Temp: {} --- Error: {}'.format(T,err))
                 plt.gca().invert_yaxis()
                 plt.show()
+    
+    #piece together a complete list of all sources again,
+    #  inserting the modeled ones where relevant
+    out_obs, out_errs = [],[]
+    for i,row in enumerate( DEC_obs ):
+        if i in matched_indexer:
+            # replace this row with modeled row
+            i_modeled = matched_indexer.index(i)
+            out_obs.append( modeled_mags[i_modeled] )
+            out_errs.append( modeled_errs[i_modeled] )
+        else:
+            out_obs.append( row[::2] )
+            out_errs.append( row[1::2] )
                 
-    return np.array(out_coords), np.array(out_mags), np.array(Ys), np.array(errors)
+    return object_coords, out_obs, out_errs
 
 
-def get_Y_all( coords, obs, err_cut=.5, plot=False):
+def get_Y_all( coords, obs, plot=False):
     '''
     Estimate all missing photometry for all sources, whether they cross-match
     or no.  (Do not bother with crossmatch!)
@@ -141,7 +177,7 @@ def get_Y_all( coords, obs, err_cut=.5, plot=False):
                 plt.scatter( MODELS[0][1:], model, c='k', alpha=.7 )
                 plt.xlabel('wavelength')
                 plt.ylabel('mag')
-                plt.title('Error: {}'.format(out_errs[i][0]))
+                plt.title('Temp: {} --- Error: {}'.format(T, err))
                 plt.gca().invert_yaxis()
                 plt.show()
     
@@ -158,28 +194,31 @@ if __name__ == '__main__':
     import os, sys
     prefix = 'catalogs/'
     for fname in os.listdir(prefix):
-        if sys.argv[1] not in fname:
+        if sys.argv[1] not in fname or 'sources' not in fname:
             continue
         print '*'*20
         print 'working on', fname
         print '*'*20
         data = np.loadtxt(prefix+fname)
         data[ data==0. ] = .00001  # hack to fix reported zero errors
+        '''
+        #for get_Y_all:
         # dec_obs = [u, u_err, g, g_err, r, r_err, i, i_err, z, z_err]
         dec_obs = np.zeros_like( data[:,:10] ) #prepare the array
         dec_obs[:,::2] = data[:,2:7]
         dec_obs[:,1::2] = data[:,8:13]
-        
         '''
-        maskA = data[:,6] < 20.  #only bother to match sources with z<20
-        good_coords = data[:,:2][ maskA ]
-        good_obs = dec_obs[ maskA ]
-        '''
+        #for get_Y_match:
+        # dec_obs = [u, u_err, g, g_err, r, r_err, i, i_err, z, z_err, y, y_err]
+        dec_obs = np.zeros_like( data[:,:12] ) #prepare the array
+        dec_obs[:,::2] = data[:,2:8]
+        dec_obs[:,1::2] = data[:,8:14]
         
-        out_coords, obj_mags, errors = get_Y_all( data[:,:2], dec_obs )
+        #out_coords, obj_mags, errors = get_Y_all( data[:,:2], dec_obs )
+        out_coords, obj_mags, errors = get_Y_match( data[:,:2], dec_obs )
         
         #save to file
-        fff = open(prefix+fname.split('.')[0]+'_all.catalog', 'w')
+        fff = open(prefix+fname.split('.')[0]+'_match.catalog', 'w')
         hhh = '# RA   DEC  mag(u g r i z y)   mag_error(u g r i z y)'
         fff.write(hhh+'\n')
         for iii,coord in enumerate(out_coords):
